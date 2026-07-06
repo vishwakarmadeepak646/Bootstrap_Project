c8764a8786cc8b7814e18a0f954b6cee5aed50b8
vishwakarmadeepak646 <vishwakarmadeepak646@gmail.com>
2026-07-06 17:24:26 +0530

    Changes

--- ORSProject04/src/main/java/com/sunilos/p4/ctl/BaseCtl.java
+++ ORSProject04/src/main/java/com/sunilos/p4/ctl/BaseCtl.java
@@ -1,6 +1,7 @@
 package com.sunilos.p4.ctl;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -158,6 +159,16 @@ public abstract class BaseCtl<B extends BaseBean, M extends BaseModel> extends H
 		} catch (DuplicateRecordException e) {
 			ServletUtility.setBean(bean, request);
 			ServletUtility.setErrorMessage(e.getMessage(), request);
+			ServletUtility.forwardPage(getView(), request, response);
+		}
+		catch (ApplicationException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage() + " ============== database is down ===================");
+			ServletUtility.setPageNo(1, request);
+			ServletUtility.setPageSize(10, request);
+			ServletUtility.setList(new ArrayList<BaseBean>(), request);
+			request.setAttribute("nextList", new ArrayList<BaseBean>());
+			ServletUtility.setErrorMessage("database service is down", request);
 			ServletUtility.forwardPage(getView(), request, response);
 		}
 
